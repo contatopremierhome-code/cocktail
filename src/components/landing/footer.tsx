dcd6362b9f716d7e1ca930534bd50a5e075bca64
Premier Home <contatopremierhome@gmail.com>
2026-01-13 21:09:49 +0000

    coloque o ponto final ao inves do ! (_for element p_)

--- src/components/landing/footer.tsx
+++ src/components/landing/footer.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
   return (
     <footer className="border-t">
       <div className="container mx-auto px-4 py-6 text-center text-muted-foreground">
-        <p>&copy; {currentYear} Cocktail Master. All Rights Reserved!</p>
+        <p>&copy; {currentYear} Cocktail Master. All Rights Reserved.</p>
       </div>
     </footer>
   );
