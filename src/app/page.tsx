f539626fa72ddff823f6ad46b269a13027f366ed
Premier Home <contatopremierhome@gmail.com>
2026-01-13 16:37:49 +0000

    Coloque os bônus e os depoimentos antes dos preços.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,9 +15,9 @@ export default function Home() {
       <main className="flex-1">
         <Hero />
         <RecipeShowcase />
-        <Pricing />
         <Bonuses />
         <Testimonials />
+        <Pricing />
         <Faq />
       </main>
       <Footer />
