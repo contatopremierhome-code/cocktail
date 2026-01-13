44fbb96a639e8ab3fbc4efe23df41642193d0f0a
Premier Home <contatopremierhome@gmail.com>
2026-01-13 22:01:21 +0000

    File changes

--- src/components/landing/main-product.tsx
+++ src/components/landing/main-product.tsx
@@ -29,7 +29,7 @@ const MainProduct = () => {
                     data-ai-hint={image.imageHint}
                 />
             )}
-            <Button asChild size="lg" className="h-14 text-lg hidden sm:inline-flex">
+            <Button asChild size="lg" className="h-14 text-lg hidden md:inline-flex">
                 <Link href="#pricing">I WANT MY GUIDE NOW!</Link>
             </Button>
         </div>
